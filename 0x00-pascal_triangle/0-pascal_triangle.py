37f251bd84db85841aac16ed313556e8c3d566ca
joyeuxclemet87 <tuyishimirejoyeuxclement@gmail.com>
2024-10-03 17:11:15 +0200

    working with printing pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,15 +2,15 @@
 """just working on the function that returns a list
 of lists of integers representing the Pascal’s triangle
 """
-def pascal_triangle(num):
+def pascal_triangle(n):
     """this is the function to returns a list of lists of int
        that representing the Pascal’s triangle
     """
     value = []
-    if (num <= 0):
+    if (n <= 0):
         return value
     value.append([1])
-    for a in range(num - 1):
+    for a in range(n - 1):
         value.append([1] + [value[a][b] + value[a][b + 1]
                     for b in range(len(value[a]) - 1)] + [1])
     return value
