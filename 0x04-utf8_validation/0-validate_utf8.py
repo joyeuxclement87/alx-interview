09f283aa8ab1fcb78005c4e025f0f7579cdc82b8
joyeuxclement87 <tuyishimirejoyeuxclement@gmail.com>
2024-10-31 21:42:07 +0200

    working with validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" working on UTF-8 Validation """
+
+
+def validUTF8(data):
+    """
+    making the method that determines if a given data set represents a valid
+    UTF-8 encoding.
+    """
+    num_byte = 0
+
+    m_1 = 1 << 7
+    m_2 = 1 << 6
+
+    for a in data:
+
+        m_byte = 1 << 7
+
+        if num_byte == 0:
+
+            while m_byte & a:
+                num_byte += 1
+                m_byte = m_byte >> 1
+
+            if num_byte == 0:
+                continue
+
+            if num_byte == 1 or num_byte > 4:
+                return False
+
+        else:
+            if not (a & m_1 and not (a & m_2)):
+                    return False
+
+        num_byte -= 1
+
+    if num_byte == 0:
+        return True
+
+    return False
